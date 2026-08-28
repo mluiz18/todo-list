d3b4e3704f70513c763045748c7b17669083347b
mluiz18 <migueluiz18@gmail.com>
2026-08-28 20:13:05 -0300

    feat: add exit option functionality and create task functionality

--- src/todolist/mluiz18/Main.java
+++ src/todolist/mluiz18/Main.java
@@ -1,8 +1,23 @@
 package todolist.mluiz18;
+
 import todolist.mluiz18.models.functions;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        functions.menu();
+        Scanner scn = new Scanner(System.in);
+        while (true) {
+            byte c = functions.menu();
+            if (c == 5) {
+                System.out.println("Até Mais");
+                break;
+            } else if (c == 1) {
+                System.out.print("Nome da Tarefa: ");
+                String name = scn.next();
+                functions.addTask(name);
+
+            }
+        }
     }
 }
