92c79c51c09ce0fca84ee92d03b1af9e435f6a23
mluiz18 <migueluiz18@gmail.com>
2026-08-28 20:01:30 -0300

    refactor: simplify menu choice processing code

--- src/todolist/mluiz18/models/functions.java
+++ src/todolist/mluiz18/models/functions.java
@@ -3,7 +3,7 @@ package todolist.mluiz18.models;
 import java.util.Scanner;
 
 public class functions {
-    public static void menu() {
+    public static byte menu() {
         Scanner scn = new Scanner(System.in);
         System.out.println("-=-=-=-=-=-= Menu -=-=-=-=-=-=");
         System.out.println("1. adicionar");
@@ -12,22 +12,13 @@ public class functions {
         System.out.println("4. remover");
         System.out.println("5. sair");
         System.out.print("Escolha: ");
-        Byte choice = scn.nextByte();
+        byte choice = scn.nextByte();
 
         if (choice < 1 || choice > 5) {
             System.out.println("Escolha Inválida! Tente Novamente.");
             menu();
-        } else if (choice == 1) {
-            System.out.println("building");
-        } else if (choice == 2) {
-            System.out.println("building");
-        } else if (choice == 3) {
-            System.out.println("building");
-        } else if (choice == 4) {
-            System.out.println("building");
-        } else {
-            System.out.println("building");
         }
+        return choice;
     }
 }
 
